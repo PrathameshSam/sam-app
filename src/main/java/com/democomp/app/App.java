6b80214bbb8ba643ce379a8f0d4d625cfa798735
prathamesh-vilas <prathamesh.vilas@rakuten.com>
2021-06-28 16:22:50 +0530

    Initial commit

---
+++ src/main/java/com/democomp/app/App.java
@@ -0,0 +1,13 @@
+package com.democomp.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
